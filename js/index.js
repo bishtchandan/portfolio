5fc0754e4a484434f45458c6aba02ba15fb86d17
Chandan Singh Bisht <chandanbisht085@gmail.com>
2021-11-01 09:20:35 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,44 @@
+const login = document.querySelector('.login-form')
+const loginBtn = document.getElementById('loginBtn');
+
+loginBtn.addEventListener('click', () => {
+    login.classList.toggle('active');
+})
+
+const searchForm = document.querySelector('.search-form');
+const searchBtn = document.getElementById('search-btn');
+
+searchBtn.addEventListener('click', () =>{
+    searchForm.classList.toggle('active');
+})
+
+
+const navbar = document.querySelector('.navbar');
+const menuBtn = document.getElementById('menu-btn');
+
+menuBtn.addEventListener('click', () =>{
+  navbar.classList.toggle('active');
+})
+
+
+
+
+
+
+
+
+var swiper = new Swiper(".projects-slider", {
+    loop: true,
+    spaceBetween: 20,
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      0: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
